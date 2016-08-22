74ee68200ef57459ed7db6d461f25f913c24835c
Addison Nishijima and Tim Bourgault <addison@>
2016-08-22 10:47:43 -0700

    add button to remove list items, add html and js for button functionality, fix syntax error

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 //business logic
 function Task(listItem, listDetails) {
-  this.listItem = item;
-  this.listDetails = details;
+  this.listItem = listItem;
+  this.listDetails = listDetails;
 }
 Task.prototype.listData = function() {
   return this.listItem + " " + this.listDetails;
 }
 // user interface logic
 $(document).ready(function() {
-  $("form#new-listItem").submit(function(event) {
+  $("form#new-entry").submit(function(event) {
     event.preventDefault();
 
     var inputtedListItem = $("input#new-list-item").val();
@@ -16,14 +16,21 @@ $(document).ready(function() {
 
     var newData = new Task(inputtedListItem, inputtedListDetails);
 
-    $("ul#tasks").append("<li><span class='task'>" + newTask.listData() + "</span></li>");
+    $("ul#tasks").append("<li><span class='task'>" + newData.listData() + "</span></li>");
 
     $(".task").last().click(function() {
     $("#show-tasks").show();
-    $("#show-tasks h2").text(newTask.listItem);
-    $(".list-item").text(newTask.listItem);
-    $(".list-details").text(newTask.listDetails);
+    $("#show-tasks h2").text(newData.listItem);
+    $(".list-item").text(newData.listItem);
+    $(".list-details").text(newData.listDetails);
   });
+
+  $("button").click(function(){
+    $("li").remove();
+    $("p").remove();
+    $("h2").remove();
+    $("button").remove();
+});
 
   $("input#new-list-item").val("");
   $("input#new-list-detail").val("");
